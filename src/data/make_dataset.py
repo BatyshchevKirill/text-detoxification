8ca85609a2f7515c0c0a66d2515b5068ba9473dd
Kirill Batyshchev <k.batyshchev@innopolis.university>
2023-11-04 18:24:54 +0300

    Add vocab building

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,22 +5,36 @@ from torch.utils.data import Dataset
 import torch
 from torch.utils.data import DataLoader, random_split, RandomSampler
 import nltk
+from torchtext.vocab import build_vocab_from_iterator
 from nltk.tokenize import word_tokenize
+from collections import Counter
+import pickle
+from tqdm import tqdm
 
 nltk.download('punkt')
 
+UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+
 
 class ToxicDataset(Dataset):
-    def __init__(self, filepath="../../data/interim/preprocessed_data.csv"):
+    def __init__(self, filepath="../../data/interim/preprocessed_data.csv", max_vocab_size=None,
+                 vocab_path="../../data/interim/vocab.pth",
+                 load_pretrained=False):
         df = pd.read_csv(filepath)
         self.toxic = df['toxic'].values.tolist()
         self.detoxed = df['detoxed'].values.tolist()
-        for i in range(len(df)):
-            self.toxic[i] = word_tokenize(self.toxic[i])
-            self.detoxed[i] = word_tokenize(self.detoxed[i])
+
+        if load_pretrained:
+            self.vocab = torch.load(vocab_path)
+        else:
+            self.vocab = build_vocab_from_iterator(self.toxic + self.detoxed,
+                                                   specials=special_symbols, max_tokens=max_vocab_size)
+            self.vocab.set_default_index(0)
+            torch.save(self.vocab, vocab_path)
 
     def __getitem__(self, i: int):
-        return self.toxic[i], self.detoxed[i]
+        return self.vocab(self.toxic[i]), self.vocab(self.detoxed[i])
 
     def __len__(self):
         return len(self.toxic)
@@ -61,15 +75,14 @@ class TransformerLoaderCreator:
         tgt_texts = list(tgt_texts)
 
         src_tokens = [text[:self.max_len] for text in src_texts]
-        src_padded = [tokens + ["<PAD>"] * (self.max_len - len(tokens)) for tokens in src_tokens]
+        src_padded = [tokens + PAD_IDX * (self.max_len - len(tokens)) for tokens in src_tokens]
         src_tensors = torch.LongTensor(src_padded)
 
         tgt_tokens = [text[:self.max_len] for text in tgt_texts]
-        tgt_padded = [tokens + ["<PAD>"] * (self.max_len - len(tokens)) for tokens in tgt_tokens]
+        tgt_padded = [tokens + PAD_IDX * (self.max_len - len(tokens)) for tokens in tgt_tokens]
         tgt_tensors = torch.LongTensor(tgt_padded)
 
         return src_tensors, tgt_tensors
 
     def __call__(self):
         return self.train_loader, self.val_loader
-
