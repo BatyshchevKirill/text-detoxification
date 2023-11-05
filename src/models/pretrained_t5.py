e5eecfb473c9b56b5547c451e7fc278a50a26613
Kirill Batyshchev <k.batyshchev@innopolis.university>
2023-11-05 16:33:06 +0300

    Bug fix

--- src/models/pretrained_t5.py
+++ src/models/pretrained_t5.py
@@ -3,11 +3,11 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 class PretrainedT5:
     def __init__(self):
-        self.model = AutoModelForSeq2SeqLM.from_pretrained("s-nlp/t5-paranmt-detox",  max_new_tokens=100)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("s-nlp/t5-paranmt-detox")
         self.tokenizer = AutoTokenizer.from_pretrained("s-nlp/t5-paranmt-detox")
 
     def __call__(self, text):
         tokens = self.tokenizer(text, return_tensors="pt")
-        out = self.model.generate(tokens["input_ids"])
+        out = self.model.generate(tokens["input_ids"],  max_new_tokens=100)
         out = self.tokenizer.decode(out[0], skip_special_tokens=True)
         return out
