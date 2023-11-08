5248678fe7d75078054bb053076533ed5a0cc157
Kirill Batyshchev <k.batyshchev@innopolis.university>
2023-11-08 13:12:27 +0300

    Update evaluate.py. Add documentation to make_dataset.py
    Signed-off-by: Kirill Batyshchev <k.batyshchev@innopolis.university>

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,44 +9,100 @@ from tqdm import tqdm
 
 nltk.download('punkt')
 
+# The constants for the vocab
 UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
 special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
 
 
 class ToxicDataset(Dataset):
-    def __init__(self, filepath="../../data/interim/preprocessed_data.csv", max_vocab_size=None,
-                 vocab_path="../../data/interim/vocab.pth",
-                 load_pretrained=False,
-                 train=True):
+    """
+    Represents the dataset of toxic text
+    """
+
+    def __init__(self,
+                 filepath: str = "../../data/interim/preprocessed_data.csv",
+                 max_vocab_size: int = None,
+                 vocab_path: str = "../../data/interim/vocab.pth",
+                 load_pretrained: bool = False,
+                 train: bool = True
+                 ):
+        """
+        :param filepath: the path to the csv file with the dataset
+        :param max_vocab_size: the maximum number of words in the vocabulary
+        :param vocab_path: the path where the vocab is stored
+        :param load_pretrained: True if we want to load the pretrained vocab
+        :param train: True if the dataset is for training, else for validating
+        """
         self.train = train
+        # Read the data
         df = pd.read_csv(filepath)
+
+        # Cast to lists
         self.toxic = df['toxic'].values.tolist()
         if train:
             self.detoxed = df['detoxed'].values.tolist()
 
+        # Tokenize the words in the sentences
         for i in tqdm(range(len(df))):
             self.toxic[i] = word_tokenize(self.toxic[i])
             if train:
                 self.detoxed[i] = word_tokenize(self.detoxed[i])
 
         if load_pretrained:
+            # Load the built vocab
             self.vocab = torch.load(vocab_path)
         else:
-            col = self.toxic + self.detoxed if train else self.toxic
-            self.vocab = build_vocab_from_iterator(col,
-                                                   specials=special_symbols, max_tokens=max_vocab_size)
+            # Create the vocab
+            col = (self.toxic + self.detoxed) if train else self.toxic
+            self.vocab = build_vocab_from_iterator(
+                col,
+                specials=special_symbols,
+                max_tokens=max_vocab_size
+            )
             self.vocab.set_default_index(0)
+
+            # Save the vocab
             torch.save(self.vocab, vocab_path)
 
     def __getitem__(self, i: int):
+        """
+        Get an item of the dataset
+
+        :param i: the index of the item
+        :return: tuple of translated sentences (toxic_sentence, detoxified_sentence) if train,
+                 if test - tuple (toxic_sentence, None)
+        """
         return self.vocab(self.toxic[i]), self.vocab(self.detoxed[i]) if self.train else None
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Number of sentences in the dataset
+
+        :return: number of sentences in the dataset, int
+        """
         return len(self.toxic)
 
 
 class TransformerLoaderCreator:
-    def __init__(self, dataset, batch_size, max_len, test_size=0.2, random_state=None):
+    """
+    Class to create dataloaders for the dataset
+    """
+
+    def __init__(
+            self,
+            dataset: ToxicDataset,
+            batch_size: int,
+            max_len: int,
+            test_size: float = 0.2,
+            random_state: int = None
+    ):
+        """
+        :param dataset: the dataset instance to create loaders from
+        :param batch_size: the size of the batch
+        :param max_len: the maximal length of the sentence
+        :param test_size: the proportion of test data, if 1, then test mode is used
+        :param random_state: the number to seed to random generators
+        """
         self.dataset = dataset
         self.batch_size = batch_size
         self.max_len = max_len
@@ -54,12 +110,21 @@ class TransformerLoaderCreator:
         self.random_state = random_state
         self.test_size = test_size
 
+        # Calculate number of train and test samples
         num_samples = len(dataset)
         num_test_samples = int(num_samples * self.test_size)
         num_train_samples = num_samples - num_test_samples
+
+        # If train mode, split the data and create train data loader
         if test_size != 1:
-            train_dataset, test_dataset = random_split(dataset, [num_train_samples, num_test_samples],
-                                                       generator=torch.Generator().manual_seed(self.random_state))
+            # Split the data
+            train_dataset, test_dataset = random_split(
+                dataset,
+                [num_train_samples, num_test_samples],
+                generator=torch.Generator().manual_seed(self.random_state)
+            )
+
+            # Create a train data loader with manual seed
             sampler = RandomSampler(train_dataset, generator=torch.Generator().manual_seed(random_state))
             self.train_loader = DataLoader(
                 train_dataset,
@@ -69,6 +134,8 @@ class TransformerLoaderCreator:
             )
         else:
             test_dataset = dataset
+
+        # Create the validation (test) data loader
         self.val_loader = DataLoader(
             test_dataset,
             batch_size=self.batch_size,
@@ -77,29 +144,58 @@ class TransformerLoaderCreator:
         )
 
     def collate_fn(self, batch):
+        """
+        The function that collates batches, preparing them to feeding to the NN
+
+        :param batch: the batch to prepare
+        :return: the prepared batches
+        """
+
         if self.test_size != 1:
+            # If in training mode, prepare target sentences
             src_texts, tgt_texts = zip(*batch)
             tgt_texts = list(tgt_texts)
+
+            # Cut too long sentences
             tgt_tokens = [text[:self.max_len - 2] for text in tgt_texts]
+
+            # Pad too short sentences, add special symbols
             tgt_padded = [
                 [BOS_IDX] + tokens + [EOS_IDX] + [PAD_IDX] * (self.max_len - len(tokens) - 2)
                 for tokens in tgt_tokens
             ]
+
+            # Convert to tensors
             tgt_tensors = torch.LongTensor(tgt_padded).transpose(1, 0)
         else:
             src_texts, _ = zip(*batch)
+
+        # Prepare input sentences
         src_texts = list(src_texts)
+
+        # Cut too long sentences
         src_tokens = [text[:self.max_len - 2] for text in src_texts]
+
+        # Pad too short sentences, add special symbols
         src_padded = [
             [BOS_IDX] + tokens + [EOS_IDX] + [PAD_IDX] * (self.max_len - len(tokens) - 2)
             for tokens in src_tokens
         ]
+
+        # Convert to tensor
         src_tensors = torch.LongTensor(src_padded).transpose(1, 0)
+
         if self.test_size != 1:
+            # Return train batch
             return src_tensors, tgt_tensors
+
+        # Return test batch
         return src_tensors
 
     def __call__(self):
+        """
+        :return: train and validation data loaders if in train mode, else test data loader
+        """
         if self.test_size == 1:
             return self.train_loader
         return self.train_loader, self.val_loader
--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,16 +1,27 @@
+import argparse
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import torch
+from src.models.preprocess import file_path
 from torchmetrics.text import BLEUScore
 from torchmetrics.text.rouge import ROUGEScore
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
 from torch.nn import Softmax
+from tqdm import tqdm
 
 
-def bleu(ref, trans):
+def bleu(input_data):
+    ref, trans = input_data
     score = BLEUScore(smooth=True)
     score = score([trans], [[ref]]).item()
     return score
 
 
-def rouge(ref, trans):
+def rouge(input_data):
+    ref, trans = input_data
     score = ROUGEScore(rouge_keys=("rouge1", "rouge2"))(trans, ref)
     rouge1_f1 = score['rouge1_fmeasure']
     rouge2_f1 = score['rouge2_fmeasure']
@@ -19,8 +30,11 @@ def rouge(ref, trans):
 
 class ToxicityClassifier:
     def __init__(self):
-        self.model = RobertaForSequenceClassification.from_pretrained('SkolkovoInstitute/roberta_toxicity_classifier')
-        self.tokenizer = RobertaTokenizer.from_pretrained('SkolkovoInstitute/roberta_toxicity_classifier')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = RobertaForSequenceClassification.from_pretrained(
+            'SkolkovoInstitute/roberta_toxicity_classifier').to(device)
+
+        self.tokenizer = RobertaTokenizer.from_pretrained('SkolkovoInstitute/roberta_toxicity_classifier').to(device)
         self.sm = Softmax(dim=0)
 
     def __call__(self, text):
@@ -30,4 +44,46 @@ class ToxicityClassifier:
         return res[1]
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate model parser")
+    parser.add_argument("predictions_path", type=file_path)
+    parser.add_argument("metric_name", choices=["bleu", "rouge", "toxicity"])
+    parser.add_argument("-r", "--reference-path", default=None, type=file_path)
+    args = parser.parse_args()
+    m_name = args.metric_name
 
+    if m_name == "bleu" or m_name == "rouge":
+        if args.reference_path is None:
+            parser.error(
+                f"Reference path is required for {m_name} score. Use -r and fill the path of the reference file"
+            )
+
+        with open(args.predictions_path, 'r') as f:
+            predictions = f.read().split("\n")
+        with open(args.reference_path, 'r') as f:
+            reference = f.read().split("\n")
+        assert len(predictions) == len(
+            reference), "The lengths of the reference and prediction files should be the same"
+        data = list(zip(predictions, reference))
+
+        metric = bleu if m_name == "bleu" else rouge
+    else:
+        with open(args.predictions_path, 'r') as f:
+            data = f.read().split("\n")
+        metric = ToxicityClassifier()
+
+    if m_name != "rouge":
+        results = 0
+
+        for item in tqdm(data):
+            results += metric(item)
+
+        print(f"Average {m_name} metric for the given texts is: {results / len(data):.4f}")
+    else:
+        res1, res2 = 0, 0
+        for item in tqdm(data):
+            res = metric(item)
+            res1 += res[0]
+            res2 += res[1]
+        print(f"Average rouge1 f1 metric for the given texts is: {res1 / len(data):.4f}")
+        print(f"Average rouge2 f1 metric for the given texts is: {res2 / len(data):.4f}")
