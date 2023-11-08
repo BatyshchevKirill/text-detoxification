9f85d174b03c3e813d35ec54799e88fa382c9b98
Kirill Batyshchev <k.batyshchev@innopolis.university>
2023-11-08 11:19:52 +0300

    Bug fix

--- src/models/checkpoint_download.py
+++ src/models/checkpoint_download.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     parser.add_argument("checkpoint_name", choices=["transformer"])
     args = parser.parse_args()
 
-    destination_path = f"models/{args.checkpoint_name}_checkpoints/{args.checkpoint_name}.pth"
+    destination_path = f"models/{args.checkpoint_name}_checkpoints/"
     os.makedirs(destination_path, exist_ok=True)
+    destination_path += "{args.checkpoint_name}.pth"
 
     if args.checkpoint_name == "transformer":
         gdown.download(transformer_25k, destination_path, quiet=False)
