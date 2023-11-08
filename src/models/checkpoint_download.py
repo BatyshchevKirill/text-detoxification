b4aa1b7232bd60a681f4c56df922d338878a0641
Kirill Batyshchev <k.batyshchev@innopolis.university>
2023-11-08 10:44:20 +0300

    Add script to download model checkpoints

---
+++ src/models/checkpoint_download.py
@@ -0,0 +1,15 @@
+import argparse
+
+import gdown
+
+transformer_25k = "https://drive.google.com/uc?id=10XRHWwdiLZuuNHEhd0L_eBrKwoAJUqd1"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train model parser")
+    parser.add_argument("checkpoint_name", choices=["transformer"])
+    args = parser.parse_args()
+
+    destination_path = f"models/{args.checkpoint_name}_checkpoints/{args.checkpoint_name}.pth"
+
+    if args.checkpoint_name == "transformer":
+        gdown.download(transformer_25k, destination_path, quiet=False)
